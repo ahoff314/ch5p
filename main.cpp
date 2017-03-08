9a6414ac75ce14a9d660534f5e0b62e1326d785c
ahoff314 <alexanderjhoffmann@gmail.com>
2017-03-07 20:55:08 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -13,21 +13,40 @@ int main() {
     int firstNum;
     int secondNum;
     int i;
+    int sum;
 
     cout << "Enter two numbers." << endl;
     cout << "First number must be less than the second number you enter." << endl;
-    cout << "Enter two numbers" << endl;
+    cout << "Enter numbers:" << endl;
 
     cin >> firstNum >> secondNum;
 
     i = firstNum;
 
-    while (i < secondNum)
+    // Output odd integers between first and second num
+    while (i <= secondNum)
     {
-        cout << i << endl;
+        if (i % 2 != 0){
+            cout << i << endl;
+        }
 
-        i = i + 1;
+        i += 1;
     }
+
+    i = firstNum;
+    sum = 0;
+
+    // Output sum of all even integers between first and second num
+    while (i <= secondNum)
+    {
+        if (i % 2 == 0){
+            sum += i;
+        }
+
+        i += 1;
+    }
+
+    cout << "Sum of even integers between " << firstNum << " and " << secondNum << " = " << sum << endl;
 
     return 0;
 }
