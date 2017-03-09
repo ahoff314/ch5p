081b2e433bfb07a7acc19351a7c4f111a2d1e6df
ahoff314 <alexanderjhoffmann@gmail.com>
2017-03-08 20:15:10 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 /***************************
  * Alex Hoffmann
- * 3/7/17
+ * 3/9/17
  * Chapter 5 Program
  ****************************/
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ int main() {
     int secondNum;
     int i;
     int sum;
+    int squareSum;
 
     cout << "Enter two numbers." << endl;
     cout << "First number must be less than the second number you enter." << endl;
@@ -30,7 +32,7 @@ int main() {
             cout << i << endl;
         }
 
-        i += 1;
+        i++;
     }
 
     i = firstNum;
@@ -43,10 +45,44 @@ int main() {
             sum += i;
         }
 
-        i += 1;
+        i++;
     }
 
     cout << "Sum of even integers between " << firstNum << " and " << secondNum << " = " << sum << endl;
 
+    // Output squares of values 1 to 10
+
+    i = 0;
+    while (i <= 10){
+        cout << i << " " << pow (i, 2) << endl;
+        i++;
+    }
+
+    // Output square of sum of odd integers
+    i = firstNum;
+    squareSum = 0;
+
+    while (i <= secondNum)
+    {
+        if (i % 2 != 0){
+            squareSum += pow(i,2);
+        }
+
+        i++;
+    }
+
+    cout << "Sum of squares of odd integers between " << firstNum << " and " << secondNum << " = " << squareSum << endl;
+
+
+    // Output all uppercase letters
+
+    i =65;
+    while (i <= 90) {
+
+        cout << static_cast<char>(i) << endl;
+        i ++;
+    }
+
     return 0;
+
 }
